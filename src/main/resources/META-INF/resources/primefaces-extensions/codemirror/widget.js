dd4c1a08a1ac4d28cbfee6cfafafa077363344f0
Thomas Andraschko <andraschko.thomas@gmail.com>
2012-03-02 20:40:07 +0000

    #115 - Adopt new PrimeFaces widget architecture

--- src/main/resources/META-INF/resources/primefaces-extensions/codemirror/widget.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/codemirror/widget.js
@@ -2,59 +2,65 @@
  * PrimeFaces Extensions CodeMirror Widget
  * 
  * @author Thomas Andraschko
- * @constructor
  */
-PrimeFacesExt.widget.CodeMirror = function(cfg) {
-	this.id = cfg.id;
-	this.cfg = cfg;
-	this.jqId = PrimeFaces.escapeClientId(this.id);
-	this.jq = $(this.jqId);
+PrimeFacesExt.widget.CodeMirror = PrimeFaces.widget.BaseWidget.extend({
+	
+	/**
+	 * Initializes the widget.
+	 * 
+	 * @param {object} cfg The widget configuration.
+	 */
+	init : function(cfg) {
+		this._super(cfg);
+	
+		//remove old instance if available
+		if (this.jq.next().hasClass('CodeMirror')) {
+			this.jq.next().remove();
+		}
+	
+		this.options = this.cfg;
+	
+		this.options.onFocus = $.proxy(function() { this.fireEvent('focus'); }, this);
+		this.options.onBlur = $.proxy(function() { this.fireEvent('blur'); }, this);
+	
+		this.options.onHighlightComplete =
+			$.proxy(function(codeMirror) { this.fireEvent('highlightComplete'); }, this);
+	
+		this.options.onChange =
+			$.proxy(function(from, to, text, next) {
+				//set value to textarea
+				this.instance.save();
+				
+				//fire event
+				this.fireEvent('change'); 
+			}, this);
+	
+		this.instance = CodeMirror.fromTextArea(this.jq[0], this.options);
+	},
 
-	//remove old instance if available
-	if (this.jq.next().hasClass('CodeMirror')) {
-		this.jq.next().remove();
+	/**
+	 * This method fires an event if the behavior was defined.
+	 *
+	 * @param {string} eventName The name of the event.
+	 * @private
+	 */
+	fireEvent : function(eventName) {
+		if (this.cfg.behaviors) {
+			var callback = this.cfg.behaviors[eventName];
+		    if (callback) {
+		    	var ext = {
+		    			params: {}
+		    	};
+	
+		    	callback.call(this, null, ext);
+		    }
+		}
+	},
+
+	/**
+	 * Returns the CodeMirror instance.
+	 */
+	getCodeMirrorInstance : function() {
+	    return this.instance;
 	}
-
-	this.options = this.cfg;
-
-	this.options.onFocus = $.proxy(function() { this.fireEvent('focus'); }, this);
-	this.options.onBlur = $.proxy(function() { this.fireEvent('blur'); }, this);
-
-	this.options.onHighlightComplete =
-		$.proxy(function(codeMirror) { this.fireEvent('highlightComplete'); }, this);
-
-	this.options.onChange =
-		$.proxy(function(from, to, text, next) {
-			//set value to textarea
-			this.instance.save();
-			
-			//fire event
-			this.fireEvent('change'); 
-		}, this);
-
-	this.instance = CodeMirror.fromTextArea(this.jq[0], this.options);
-
-	this.postConstruct();
-}
-
-PrimeFaces.extend(PrimeFacesExt.widget.CodeMirror, PrimeFaces.widget.BaseWidget);
-
-/**
- * This method fires an event if the behavior was defined.
- *
- * @author Thomas Andraschko
- * @param {string} eventName The name of the event.
- * @private
- */
-PrimeFacesExt.widget.CodeMirror.prototype.fireEvent = function(eventName) {
-	if (this.cfg.behaviors) {
-		var callback = this.cfg.behaviors[eventName];
-	    if (callback) {
-	    	var ext = {
-	    			params: {}
-	    	};
-
-	    	callback.call(this, null, ext);
-	    }
-	}
-}
+});
